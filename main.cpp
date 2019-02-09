ad6d4baf63d0b02883c638cf50c126d13a22961b
Equan Pr <equan.p@gmail.com>
2019-02-09 18:39:38 +0700

    Simple Game Loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 // main.cpp
 #include <sqlite3.h>
 #include <stdio.h>
-#include <SDL2/SDL.h> 
+#include <SDL2/SDL.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -9,22 +10,38 @@ int main(int argc, char *argv[])
 
   SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow(
-    "Game",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
-
+  SDL_Window *window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  SDL_SetRenderDrawColor(renderer, 123, 212, 89, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
 
-  SDL_Delay(5000);
+  SDL_Event input;
+  bool quit = false;
+  int r = 0;
+  int g = 255;
+  int b = 100;
 
+  // Game Loop
+  while (!quit)
+  {
+    while (SDL_PollEvent(&input) > 0)
+    {
+      if (input.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+      else if (input.type == SDL_KEYDOWN)
+      {
+        r = rand() % 255;
+        g = rand() % 255;
+        b = rand() % 255;
+      }
+
+      SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
+      SDL_RenderClear(renderer);
+      SDL_RenderPresent(renderer);
+    }
+  }
+
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
